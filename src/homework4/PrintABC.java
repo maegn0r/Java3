1c189434b8c12e68a6e527dc0bd672cfbaca29aa
novitskynick <novitskynick@gmail.com>
2021-08-26 09:32:27 +0300

    add homework4 part print abc

---
+++ src/homework4/PrintABC.java
@@ -0,0 +1,70 @@
+package homework4;
+
+public class PrintABC {
+
+    private volatile char currentLetter = 'A';
+    private final Object monitor = new Object();
+
+    public static void main(String[] args) {
+        PrintABC waitNotifyApp = new PrintABC();
+        new Thread(waitNotifyApp::printA).start();
+        new Thread(waitNotifyApp::printB).start();
+        new Thread(waitNotifyApp::printC).start();
+    }
+
+    public void printA() {
+        synchronized (monitor) {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    while (currentLetter != 'A') {
+                        monitor.wait(5000);
+                    }
+                    System.out.println("A");
+                    currentLetter = 'B';
+                    monitor.notifyAll();
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    public void printB() {
+        synchronized (monitor) {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    while (currentLetter != 'B') {
+                        monitor.wait(5000);
+                    }
+                    System.out.println("B");
+                    currentLetter = 'C';
+                    monitor.notifyAll();
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+    public void printC() {
+        synchronized (monitor) {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    while (currentLetter != 'C') {
+                        monitor.wait(5000);
+                    }
+                    System.out.println("C");
+                    currentLetter = 'A';
+                    monitor.notifyAll();
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
